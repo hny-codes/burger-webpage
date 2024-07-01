3b8fd8ec312777a3b78f1e5a3d0ca39f08ffca54
Henry <hnguyhenry@protonmail.com>
2024-06-30 23:18:37 -0700

    Add HotSection to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "@/components/hero/HeroSection";
+import HotSection from "@/components/hot-items/HotSection";
 
 export default function Home() {
   return (
     <main className=''>
       <HeroSection />
+      <HotSection />
     </main>
   );
 }
