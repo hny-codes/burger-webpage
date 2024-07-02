871a8fe13519a756b863190ed6170605e543480d
Henry <hnguyhenry@protonmail.com>
2024-07-01 21:23:23 -0700

    Add subtext to section title

--- src/components/hot-items/HotSection.tsx
+++ src/components/hot-items/HotSection.tsx
@@ -1,12 +1,16 @@
-import BurgerCarousel from "./BurgerCarousel";
+import BurgerCarousel from './BurgerCarousel';
 
 export default function HotSection() {
   return (
-    <section className='mt-20 md:mt-80 mb-40 font-poppins text-white'>
+    <section className='mt-20 md:mt-96 md:pt-20 mb-40 font-poppins text-white'>
       <div>
-        <h2 className='capitalize font-semibold text-3xl text-center'>
-          Hot Items
-        </h2>
+        <div className='text-center uppercase space-y-10 px-8'>
+          <h2 className=' font-semibold text-3xl lg:text-5xl'>— Hot Items —</h2>
+          <h3 className='text-xl lg:text-3xl '>
+            Locally sourced, organic ingredients for a fresh and eco-friendly
+            experience
+          </h3>
+        </div>
         <BurgerCarousel />
       </div>
     </section>
