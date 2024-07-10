e6663b05e64952bc629fab4db5e69c861e2ac87b
Henry <hnguyhenry@protonmail.com>
2024-07-09 22:00:15 -0700

    Adjust padding

--- src/components/feedback/FeedbackSection.tsx
+++ src/components/feedback/FeedbackSection.tsx
@@ -4,7 +4,7 @@ export default function FeedbackSection() {
   return (
     <section className='text-white px-0 pb-32'>
       <div>
-        <h2 className='text-3xl text-center uppercase py-16 lg:text-5xl font-semibold mt-8 sm:mt-32'>
+        <h2 className='text-3xl text-center uppercase pt-16 xl:pb-16 lg:text-5xl font-semibold mt-8 sm:mt-32'>
           — What our customers say —
         </h2>
         <div>
